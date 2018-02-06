1dab6032a2aa88e576a7bfd3d7bfb4a522a1db89
Shifa Haider <shifa.haider065@gmail.com>
2018-02-06 15:07:21 +0500

    work in firebase

--- src/app.js
+++ src/app.js
@@ -1,1 +1,11 @@
 
+var db = firebase.firestore();
+function addTodo(){
+    var inp = document.getElementById('inp');
+    console.log(inp.value);
+    db.collection('todos').add({
+        time : Date.now(),
+        todo : inp.value
+    });
+    inp.value = '';
+}
