7b505f1bb81bd11e5b88fba313d1942c56ec32dd
Shifa Haider <shifa.haider065@gmail.com>
2018-02-06 15:30:25 +0500

    firebase todo work is completed

--- src/app.js
+++ src/app.js
@@ -9,3 +9,20 @@ function addTodo(){
     });
     inp.value = '';
 }
+function addFruits(){
+    var fruitInp = document.getElementById('fruitInp');
+    console.log(fruitInp.value);
+    db.collection('fruits').add({
+        fruitName: fruitInp.value,
+        time: Date.now()
+    });
+    fruitInp.value = '';
+}
+function addCountry(){
+    var conInp = document.getElementById('conInp');
+    db.collection('countries').add({
+        countryName : conInp.value,
+        time : Date.now()
+    });
+    conInp.value = '';
+}
