0f4a752ef31e0612b73cc10c80d69c16effffe72
Shifa Haider <shifa.haider065@gmail.com>
2018-02-07 17:41:02 +0500

    complete firebase second app

--- src/app.js
+++ src/app.js
@@ -93,6 +93,7 @@ function getCities(){
         })
     })
 }
+
 function addFlowers(){
     var flowInp = document.getElementById('flowInp');
     console.log(flowInp.value);
@@ -101,6 +102,19 @@ function addFlowers(){
         time : Date.now()
     });
     flowInp.value = '';
+}
+getFlower();
+function getFlower(){
+    var flower = document.getElementById('flower');
+    db.collection('flowers').get().then(function(collectFlower){
+        console.log(collectFlower);
+        collectFlower.forEach(function(doc){
+            var d = doc.data();
+            var li = document.createElement('li');
+            li.innerHTML = d.flowerName + ' ' + new Date(d.time).toLocaleString();
+            flower.appendChild(li);
+        })
+    })
 }
 
 function addVegetables(){
@@ -112,6 +126,19 @@ function addVegetables(){
     });
     vegInp.value = '';
 }
+getVegetable();
+function getVegetable(){
+    var veg = document.getElementById('veg');
+    db.collection('vegetables').get().then(function(collectVegetable){
+        console.log(collectVegetable);
+        collectVegetable.forEach(function(doc){
+            var d = doc.data();
+            var li = document.createElement('li');
+            li.innerHTML = d.vegetableName + ' ' + new Date(d.time).toLocaleString();
+            veg.appendChild(li);
+        })
+    })
+}
 function addPhones(){
     var phoneInp = document.getElementById('phoneInp');
     console.log(phoneInp.value);
@@ -121,23 +148,62 @@ function addPhones(){
     });
     phoneInp.value = '';
 }
+getPhones();
+function getPhones(){
+    var phones = document.getElementById('phone');
+    db.collection('Smart phones').get().then(function(collectPhones){
+        console.log(collectPhones);
+        collectPhones.forEach(function(doc){
+            var d = doc.data();
+            var li = document.createElement('li');
+            li.innerHTML = d.phonesName + ' ' + new Date(d.time).toLocaleString();
+            phones.appendChild(li);
+        })
+    })
+}
 function addNames(){
     var nameInp = document.getElementById('nameInp');
     console.log(nameInp.value);
     db.collection('Name').add({
-        namesName : nameInp.value,
+        names: nameInp.value,
         time : Date.now()
     });
     nameInp.value = '';
+}
+getNames();
+function getNames(){
+    var names = document.getElementById('name');
+    db.collection('Name').get().then(function(collectNames){
+        console.log(collectNames);
+        collectNames.forEach(function(doc){
+            var d = doc.data();
+            var li = document.createElement('li');
+            li.innerHTML = d.names + ' ' + new Date(d.time).toLocaleString();
+            names.appendChild(li);
+        })
+    })
 }
 function addBooks(){
     var bookInp = document.getElementById('bookInp');
     console.log(bookInp.value);
     db.collection('Books').add({
-        names : bookInp.value,
+        books : bookInp.value,
         time : Date.now()
     });
     bookInp.value = '';
+}
+getBooks();
+function getBooks(){
+    var books = document.getElementById('book');
+    db.collection('Books').get().then(function(collectBooks){
+        console.log(collectBooks);
+        collectBooks.forEach(function(doc){
+            var d = doc.data();
+            var li = document.createElement('li');
+            li.innerHTML = d.books + ' ' + new Date(d.time).toLocaleString();
+            books.appendChild(li);
+        })
+    })
 }
 function addColours(){
     var colourInp = document.getElementById('colourInp');
@@ -148,12 +214,38 @@ function addColours(){
     });
     colourInp.value = '';
 }
+getColours();
+function getColours(){
+    var colours = document.getElementById('colour');
+    db.collection('colours').get().then(function(collectColours){
+        console.log(collectColours);
+        collectColours.forEach(function(doc){
+            var d = doc.data();
+            var li = document.createElement('li');
+            li.innerHTML = d.colourName + ' ' + new Date(d.time).toLocaleString();
+            colours.appendChild(li);
+        })
+    })
+}
 function addGames(){
     var gameInp = document.getElementById('gameInp');
     console.log(gameInp.value);
     db.collection('games').add({
-        FavGameName : gameInp.value,
+        favGameName : gameInp.value,
         time : Date.now()
     });
     gameInp.value = '';
+}
+getGames();
+function getGames(){
+    var games = document.getElementById('game');
+    db.collection('games').get().then(function(collectGames){
+        console.log(collectGames);
+        collectGames.forEach(function(doc){
+            var d = doc.data();
+            var li = document.createElement('li');
+            li.innerHTML = d.favGameName + ' ' + new Date(d.time).toLocaleString();
+            games.appendChild(li);
+        })
+    })
 }
